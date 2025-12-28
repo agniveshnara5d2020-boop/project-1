2373133972f2bbfb39992a3211f7869a2e60879a
agniveshnara5d2020-boop <agniveshnara5d2020@gmail.com>
2025-12-28 17:19:53 +0000

    percentage

---
+++ activity4.py
@@ -0,0 +1,9 @@
+print("enter marks obtained in 4 subjects:")
+math = int(input("maths:"))
+english = int(input("english:"))
+science = int(input("science:"))
+hindi = int(input("hindi:"))
+sum = math+english+science+hindi
+print("sum")
+perc=(sum/400)*100
+print(perc)
