03f4d4e0ca6d19a7f86ed1510bc8fa418c56dd60
agniveshnara5d2020-boop <agniveshnara5d2020@gmail.com>
2026-01-09 17:23:32 +0000

    membership operators

---
+++ activity15.py
@@ -0,0 +1,16 @@
+a = [1,2,3,4,5]
+b = [1,2,3,4,5]
+c = a 
+
+print(a in c )
+print(a not in b)
+print(a in c )
+print(a not in b )
+
+
+
+sentence = "hello world"
+
+print('hello'in sentence)
+print('python'in sentence)
+print('xyz'not in sentence)
