606e1aff55c6e73b96a39431344e8796e355f55d
agniveshnara5d2020-boop <agniveshnara5d2020@gmail.com>
2026-01-16 17:28:28 +0000

    correct mean

---
+++ activity18.py
@@ -0,0 +1,10 @@
+mean1 = 38
+wn = 36
+cn = 56
+tn = 40 
+sum = mean1*tn
+print("the sum of 40 numbers is", sum)
+num2 = sum - ((wn)-(cn))
+print("sum-((wn)-(cn)):", num2 )
+mean2 = num2/tn
+print(mean2)
