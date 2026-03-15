2e8e66650b693bc0896dac5cd88309a8d0e72fff
agniveshnara5d2020-boop <agniveshnara5d2020@gmail.com>
2026-03-15 15:24:48 +0000

    square project

---
+++ activity41.py
@@ -0,0 +1,13 @@
+import turtle
+screen = turtle.Screen()
+screen.bgcolor("blue")
+
+draw = turtle.Turtle()
+
+for i in range(4):
+    draw.forward(250)
+    draw.right(90)
+
+turtle.done()
+
+               
